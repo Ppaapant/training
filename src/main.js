d26ac3474c8cd5297652d8e857d54cb7ddce672a
Papant <andriy.chornenko@ukr.net>
2025-01-31 15:32:56 +0200

    n

--- src/main.js
+++ src/main.js
@@ -34,13 +34,16 @@ const getBookbyid = bookId => {
     console.log(err);
   });
 
-  // ------------------------------------------------------
+  // ------------------Method POST------------------------------------
 
 
   const addBook = bookInfo => {
     const fetchOptions = {
       method: 'POST',
-      body: bookInfo,
+      body: JSON.stringify(bookInfo),
+      headers: {
+        'content-type': 'json'
+      }
     }
   }
   addBook({
@@ -52,4 +55,43 @@ const getBookbyid = bookId => {
 
 
 
-  
+  // --------------------------------------------Update (PUT/PATH)----------------------------------------------
+
+  const updatebookById = (bookInfoById, bookId) => {
+    const fetchOption = {
+    method: 'PUT',
+    body: JSON.stringify(bookInfoById),
+    headers: {
+     'content-type': 'application/json',
+    },
+    };
+
+    return fetch('https://6788776.ffrg.rggr/books/${bookId}', {}).then(response => {
+   if(!response.ok) {
+    throw new Error(response.status)
+   }
+
+   return response.json();
+    });
+  };
+
+  updatebookById({ title: 'Some books' }, 9)
+   .then(data => {
+    console.log(data);
+   })
+   .catch(err => {
+    console.log(err);
+   });
+
+   // ---------------------------------------------------------------DELETE-----------------------------------------------------------------------
+
+
+   const deletedByID =  bookID => {
+    return fetch().then( response => {
+      if(!response.ok){
+      throw new Error(response.status)
+      }
+      return response.json();
+    });
+   };
+ 
